6211cb216f9ffab51d9be3a429707034376ee3e4
Santhosh <santho-sh@outlook.com>
2025-01-21 14:01:32 +0530

    Update root endpoint to return listings data

--- server.js
+++ server.js
@@ -1,40 +1,12 @@
 const express = require("express");
+const listings = require("./data");
 
 const app = express();
 const port = 5000;
 
 // Test endpoints
 app.get("/", (req, res) => {
-  res.json({ message: "Hello World from node app!" });
-});
-
-app.get("/api/test", (req, res) => {
-  res.json({ message: "GET request successful", timestamp: new Date() });
-});
-
-app.post("/api/test", (req, res) => {
-  res.json({
-    message: "POST request successful",
-    body: req.body,
-    timestamp: new Date(),
-  });
-});
-
-app.put("/api/test/:id", (req, res) => {
-  res.json({
-    message: "PUT request successful",
-    id: req.params.id,
-    body: req.body,
-    timestamp: new Date(),
-  });
-});
-
-app.delete("/api/test/:id", (req, res) => {
-  res.json({
-    message: "DELETE request successful",
-    id: req.params.id,
-    timestamp: new Date(),
-  });
+  res.json({ message: "Hello World from node app!", data: listings });
 });
 
 app.listen(port, () => {
